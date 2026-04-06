d0cd255dce312d876bd82e2b7967807f030f6677
Gakuseei <erikschaefer07@icloud.com>
2026-04-06 23:36:04 +0200

    reduce noisy dev logs

--- main.js
+++ main.js
@@ -233,7 +233,9 @@ function createWindow() {
   // Load the app
   if (process.env.NODE_ENV === 'development') {
     mainWindow.loadURL('http://localhost:3000');
-    mainWindow.webContents.openDevTools();
+    if (process.env.ARIA_OPEN_DEVTOOLS === '1') {
+      mainWindow.webContents.openDevTools();
+    }
   } else {
     mainWindow.loadFile(path.join(__dirname, 'dist/index.html'));
   }
